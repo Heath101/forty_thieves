1e667acfc14ccb5c6414db83eddd609b5655ee20
Heath Attig <heath.attig@gmail.com>
2016-09-15 15:29:13 -0600

    refactor column to store cards

--- src/js/Column.js
+++ src/js/Column.js
@@ -3,11 +3,11 @@ require('../styles/column.scss')
 export default class Column {
   constructor(el) {
     this.el = document.getElementById(el)
-    this.level = 0
+    this.cards = []
     this.attach()
   }
 
-  attach() {}
+  attach() { }
 
   contains(x,y) {
     let bounds = this.el.getBoundingClientRect()
@@ -15,10 +15,11 @@ export default class Column {
   }
 
   addCard(card) {
-    let vertOffset =  50 * this.level
-    card.el.style.zIndex = this.level + 10
+    let level = this.cards.length
+    this.cards.push(card)
+    let vertOffset =  50 * level
+    card.el.style.zIndex = level + 10
     card.el.style.left = this.el.offsetLeft + 'px'
     card.el.style.top = this.el.offsetTop + vertOffset + 'px'
-    this.level += 1
   }
 }
