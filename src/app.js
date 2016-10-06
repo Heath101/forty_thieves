968a0d188e100a69e7b717c497b45b8011accd49
Heath Attig <heath.attig@gmail.com>
2016-10-06 10:21:50 -0600

    add a start button

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ import MoveList from './js/MoveList.js'
 
 window.moveList = new MoveList()
 
-let game = new FortyThieves()
-game.init(document.getElementById('game'))
+const start = document.getElementById('start')
+start.addEventListener('click', startGame)
+
+function startGame(e) {
+  let game = new FortyThieves()
+  game.init(document.getElementById('game'))
+  console.log("Game Started");
+  e.target.removeEventListener(e.type, startGame)
+}
 
 let undo = document.getElementById('undo')
 undo.addEventListener('click', e => {
